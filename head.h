2f6cb1e7225387710a3f77cee0fa097dd1b4807b
wangjian <wangjian@wangjian-pc.(none)>
2014-05-29 22:27:51 +0800

    change tap

--- head.h
+++ head.h
@@ -19,60 +19,60 @@
  
 /*服务器接收消息后，创建的在线用户列表*/
 struct user_info{
-       char user_name[20];
-       int  id;
-       struct sockaddr_in cli_addr;
-       struct user_info *next;
+	char user_name[20];
+	int  id;
+	struct sockaddr_in cli_addr;
+	struct user_info *next;
 };
 //分组信息
 struct group_info
 {
-       char group_name[20];
-       int  id;
-	   struct user_info *owner;
-       struct group_info *next;  
+	char group_name[20];
+	int  id;
+	struct user_info *owner;
+	struct group_info *next;  
 };
 
 /*客户端给服务器发送的消息*/
 struct msg {
-       char type;
-       char self_name[20];
-       char dst_name[20];	
-       char data[MAXSIZE];
+	char type;
+	char self_name[20];
+	char dst_name[20];	
+	char data[MAXSIZE];
 };
 /*消息类型定义*/
 enum msg_type{           
-       LOG_IN = 1,     //登录
-	   LOGIN_TOALL,    //登陆提示消息
-       REFRESH,         //要求刷新用户在线用户（重新打印在线用户）
-       CHAT_PRI,        //私聊消息
-       CHAT_ALL,        //群聊消息
-       DOWNLOAD,        //从服务器下载文件（下载之前先打印文件列表）
-       UPLOAD,         //上传文件到服务器
-       OFFLINE,         //下线通知
-       OVER,            //服务器发送本次消息结束
-       ERROR,           //重复登录
-       FILE_NAME,       //发送文件列表
-       FILE_ERROR,     //选择文件名失败
-	  SERVER_EXIT,     //服务器退出信息
-	   CREATE_GROUP,    //创建组
-	   JOIN_GROUP,      // 加入组
-	   LIST_GROUP,      // 列出存在的组
-	   GROUP_CHAT,     //组内对话
-	   GROUP_DELETE    //删除组
+	LOG_IN = 1,     //登录
+	LOGIN_TOALL,    //登陆提示消息
+	REFRESH,         //要求刷新用户在线用户（重新打印在线用户）
+	CHAT_PRI,        //私聊消息
+	CHAT_ALL,        //群聊消息
+	DOWNLOAD,        //从服务器下载文件（下载之前先打印文件列表）
+	UPLOAD,         //上传文件到服务器
+	OFFLINE,         //下线通知
+	OVER,            //服务器发送本次消息结束
+	ERROR,           //重复登录
+	FILE_NAME,       //发送文件列表
+	FILE_ERROR,     //选择文件名失败
+	SERVER_EXIT,     //服务器退出信息
+	CREATE_GROUP,    //创建组
+	JOIN_GROUP,      // 加入组
+	LIST_GROUP,      // 列出存在的组
+	GROUP_CHAT,     //组内对话
+	GROUP_DELETE    //删除组
 };
 /*服务器总列表*/
 struct servmsg
 {
-       struct msg recvmsg;
-       struct sockaddr_in addr;
-       struct servmsg *next;
+	struct msg recvmsg;
+	struct sockaddr_in addr;
+	struct servmsg *next;
 };
 
 /*建立UDP套接字*/
 int udp_link(void)
 {
-      int sock_fd;
-       sock_fd = socket(AF_INET,SOCK_DGRAM,0);
-       return sock_fd;     
+	int sock_fd;
+	sock_fd = socket(AF_INET,SOCK_DGRAM,0);
+	return sock_fd;     
 }
